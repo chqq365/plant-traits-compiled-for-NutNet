3470728aaa8b00558575e065ca0100a1f3f8270c
Chen <aj01baqo@idiv.de>
2023-03-07 14:00:23 +0100

    major revision; simplify the codes, delete vairance component analyses; delete cwm and fd for each trait

--- compling plant traits from TRY, AusTraits, BIEN, and NutNet_3.R
+++ compling plant traits from TRY, AusTraits, BIEN, and NutNet_3.R
@@ -3,7 +3,7 @@
 ## add root traits too from Groot (did not include due to lack of standardized species names)
 
 ## for TRY, data (Height, LA, Leaf C, Leaf N, Leaf P,  Seed dry mass, SLA) were downloaded 
-## February 13 2013 (Request No: 24964)
+## February 13 2023 (Request No: 24964)
 
 ## focus on Height, LA, Leaf C, Leaf N, Leaf P,  Seed dry mass, SLA
 rm(list=ls())
@@ -33,6 +33,7 @@ unique(tr1$TraitName)
 colnames(tr1)
 # add location information; find data for latitude, longitude and altitude under DataIDs 59, 60 and 61 as suggested by Jens Kattge
 check.names<-unique(tr1$DataName)
+
 tr2.location<-tr1%>%select(ObservationID, DataID, DataName, OriglName, OrigValueStr, StdValue)%>%
   filter(DataID%in%c(59, 60, 61))%>%distinct()
 table(tr2.location$DataName)
@@ -70,7 +71,7 @@ unique(che.vege$UnitName)##  "m"
 table(che.vege$ValueKindName)# mainly measured by Single
 ## include both generative and vegetative height
 ## delete trait names that we are not interested 
-tr3<-tr3[-grep("content per leaf area", tr3$TraitName),]
+tr3<-tr2[-grep("content per leaf area", tr2$TraitName),]
 unique(tr3$TraitName)
 
 ## adjust the names for traits 
@@ -107,10 +108,10 @@ check.species.restrition<- unique(check.restrition$standard_taxon)
 # delete datasetID that require authorships 
 tr5.authorship.free<-tr5%>%filter(!DatasetID %in% author.req$DatasetID)
 check.species.free<- unique(tr5.authorship.free$standard_taxon)
-check.unique.species<-check.species.restrition[!check.species.restrition%in%check.species.free]
-length(check.unique.species)/length(check.species.restrition)
-# around 23% of the species are unique that would be deleted if we do not include those data sets require authorships
-# therefore better to include those data sets that require authorships
+check.unique.species.not.free<-check.species.restrition[!check.species.restrition%in%check.species.free]
+length(check.unique.species.not.free)/length(check.species.free)
+# very few unique species are added from those restricted data
+# therefore better to exclude those data sets that require authorships
 
 ###################################################################################################
 ################################## add traits from BIEN  ##########################################
@@ -186,7 +187,7 @@ au1<-au%>%filter(trait_name%in%traits.list)%>%merge(site.inf, by=c("dataset_id",
 table(au1$trait_name)
 
 ## check whether each trait name has only one unit 
-che.unit<-au%>%filter(trait_name%in%traits.list)%>%select(trait_name, unit)%>%distinct()
+(che.unit<-au%>%filter(trait_name%in%traits.list)%>%select(trait_name, unit)%>%distinct())
 # attention, leaf_mass_per_area is 1/SLA
 ## add the units and indicate that trait data were collected from Australia
 # delete repeats
@@ -204,8 +205,7 @@ colnames(l.nutnet)
 l.nutnet1<-l.nutnet%>%
   select("continent",   "country", "region",  "site_code", "block",  "plot", "trt", "year", "latitude",   "longitude", "Taxon", "leaf_pct_N", "leaf_pct_C", "leaf_pct_P",  "leaf_pct_K", "SLA_v2" )%>%
   pivot_longer(cols = leaf_pct_N:SLA_v2)%>%
-  arrange(name)%>% mutate(units=rep(c("mm^2 / g", "%","%","%","%"), each=2664))%>%
-  filter(!is.na(value))%>%mutate(database="NutNet")%>%unique()
+  mutate(units=ifelse(name %in% c("SLA_v2"), "mm^2 / g", "%"))%>%filter(!is.na(value))%>%mutate(database="NutNet")%>%unique()
 # also add root traits 
 l.nut.root<-read.csv(paste0(dir.data, "root-biomass-Cleand-et-al-2019.csv"))
 # seems like root biomass data at the community level, this does not help. 
@@ -222,14 +222,14 @@ colnames(groots)
 groots1<-groots%>%select("family", "genus", "species", "familyTNRS", "genusTNRS", "speciesTNRS", "traitName",  "traitValue", "errorRiskEntries", "errorRisk", "location", "decimalLatitude", "decimalLongitud", "elevation")                      
 # check species 
 check.species<-groots1%>%select("family", "genus", "species", "familyTNRS", "genusTNRS", "speciesTNRS")%>%distinct()
-# really weird species names  
+# really weird species names  (also do not understand what is speciesTNRS)
 ###################################################################################################
 ################################## combine all traits together#####################################
 ###################################################################################################
 ## unify species name, trait name, and trait unit
-colnames(tr5)
-(uni.try.nu<-tr5%>%group_by(TraitName1, UnitName)%>%select(TraitName1, UnitName)%>%distinct())
-tr6<-tr5%>%select(database, Latitude, Longitude, Altitude, standard_taxon, TraitName1, UnitName, StdValue)
+colnames(tr5.authorship.free)
+(uni.try.nu<-tr5.authorship.free%>%group_by(TraitName1, UnitName)%>%select(TraitName1, UnitName)%>%distinct())
+tr6<-tr5.authorship.free%>%select(database, Latitude, Longitude, Altitude, standard_taxon, TraitName1, UnitName, StdValue)
 ###################################################################################################
 ## for data from Australia 
 colnames(au2)
@@ -248,9 +248,8 @@ au2$trait_name1[au2$trait_name=="leaf_mass_per_area"]<-"SLA"
 au2$trait_name1[au2$trait_name=="seed_dry_mass"]<-"Seed dry mass"
 unique(au2$trait_name1)
 ## capitalize the species names 
-au2$standard_taxon<-toupper(au2$taxon_name)
 colnames(au2)
-au3<-au2%>%mutate(value0=as.numeric(value))%>%mutate(value1=ifelse(trait_name1=="SLA", 1/(value0/1000), value0))%>%
+au3<-au2%>%mutate(standard_taxon=toupper(taxon_name), value0=as.numeric(value))%>%mutate(value1=ifelse(trait_name1=="SLA", 1/(value0/1000), value0))%>%
   select(database, latitude, longitude, altitude, standard_taxon, trait_name1, unit, value1)
 names(au3)<-c("database", "Latitude", "Longitude", "Altitude", "standard_taxon", "TraitName1", "UnitName", "StdValue")
 check.sla<-au3%>%filter(TraitName1=="SLA")
@@ -314,9 +313,6 @@ all.traits<-tr6%>%bind_rows(y=au3)%>%bind_rows(y=tr.bien3)%>%bind_rows(y=l.nutne
 
 # add continent information to the data using the following function
 # https://stackoverflow.com/questions/21708488/get-country-and-continent-from-longitude-and-latitude-point-in-r
-library(rworldmap) 
-library(sp)
-
 coords2continent = function(points)
 {  
   countriesSP <- getMap(resolution='low')
@@ -352,61 +348,6 @@ all.traits1<-all.traits%>%merge(y=points, by=c("Longitude", "Latitude"))%>%
 # check trait value distribution
 ggplot(all.traits1)+geom_histogram(aes(x=StdValue))+facet_wrap(~TraitName1, scales="free")
 # fwrite(all.traits1, file ="combining traits at individual level from TRY, BIEN, Aus, and NutNet.csv" )
-
-###################################################################################################
-################################# variance component analyses########################################
-###################################################################################################
-check.sample.size<-all.traits1%>%
-  mutate(sample.location=paste(as.character(Latitude), as.character(Longitude), sep="_"))%>%group_by(standard_taxon, TraitName1, sample.location, continent, country)%>%
-  summarise(N=length(StdValue))%>%filter(N>=10)%>%mutate(id=paste0(standard_taxon, TraitName1, sample.location))
-length(unique(check.sample.size$standard_taxon))
-
-# for each sampling location, randomly select 10 records 
-all.traits1_ran<-all.traits1%>%
-  mutate(sample.location=paste(as.character(Latitude), as.character(Longitude), sep="_"))%>%mutate(id=paste0(standard_taxon, TraitName1,  sample.location))%>%filter(id%in%check.sample.size$id)%>%
-  group_by(standard_taxon, TraitName1,  sample.location,  continent, country)%>%sample_n(10, replace = F)
-table(all.traits1_ran$TraitName1)
-
-check.sample.size1<-all.traits1_ran%>%group_by(standard_taxon, TraitName1,  sample.location, continent, country)%>%
-  summarise(N=length(StdValue))%>%arrange(standard_taxon, TraitName1, N)
-range(check.sample.size1$N)
-
-# check variance components analyses (this is one of the projects I working on)
-library(nlme);library(lmerTest)
-
-variance.explain<-c()
-
-for(tr in unique(all.traits1_ran$TraitName1)){ 
-  # tr<-"Height"
-  temp.data<-all.traits1_ran%>%filter(TraitName1==tr)%>%mutate(genus=gsub( " .*$", "", standard_taxon))
-  sample.n<-nrow(temp.data)
-  sample.n1<-temp.data%>%group_by(continent, country, sample.location)%>%mutate(N=length(StdValue))
-  
-  #mod<-lme(StdValue~1, random = ~1|genus/standard_taxon/continent/country, data=temp.data)
-  #summary(mod)
-  mod1<-lmer(StdValue~1+(1|genus/standard_taxon/continent/country), data=temp.data)
-  sum.t<-summary(mod1)
-  variance<-as.data.frame(sum.t$varcor)
-  variance1<-variance%>%select(grp, vcov)%>%mutate(total.v=sum(vcov), variance.component=vcov/total.v*100)%>%
-    select(grp, variance.component)%>%mutate(TraitName1=tr, sample.n=sample.n)
-  variance.explain<-rbind(variance.explain, variance1)
-}
-unique(variance.explain$grp)
-levels(variance.explain$grp)
-variance.explain1<-variance.explain%>%mutate(grp1=ifelse(grp=="country:(continent:(standard_taxon:genus))", "country:(continent:(standard_taxon:genus))", grp),
-                                             TraitName2=ifelse(TraitName1=="Seed dry mass", "Seed mass", TraitName1),
-                                             TraitName.and.sample=paste(TraitName2, sample.n, sep="_"))%>%
-  filter(!grepl("root|Aboveground", TraitName.and.sample))
-
-variance.explain1$grp1<-factor(variance.explain1$grp1, levels=c("genus", "standard_taxon:genus", "continent:(standard_taxon:genus)", "country:(continent:(standard_taxon:genus))", "Residual"))
-
-(pp.variance<-ggplot(variance.explain1, aes(TraitName.and.sample, variance.component, fill=grp1))+theme_bw(base_size=18)+
-    geom_bar(stat = "identity", position = "stack")+
-    labs(x="trait names", y="variance explained (%)", fill="levels")+
-    coord_flip())
-# ggsave(pp.variance, width=13.3, file="variance explained for species that occur in NutNet sites based on traits from global databases.png")
-
-# continent did not explain much of the trait variations 
 
 
  ###################################################################################################
@@ -451,7 +392,7 @@ table(all.traits.for.nutnet.avg.1$TraitName1) # root traits and Aboveground biom
 sp.level.trait.nutnet<-all.traits.for.nutnet.avg.1%>%filter(standard_taxon%in%all.spp)
 
 
-# fill NAs for each trait 
+# fill NAs for species for each trait without species-level trait value
 all.traits1a<-all.traits1[-grep("root|Aboveground", all.traits1$TraitName1),]# excluded root traits  
 all.traits2<-all.traits1a%>%mutate(genus=gsub( " .*$", "", standard_taxon)) # add genus data
 unique(all.traits2$TraitName1)
@@ -464,7 +405,7 @@ for(tr in unique(all.traits2$TraitName1)){
     mutate(genus=gsub( " .*$", "", standard_taxon))
   for(g in unique(fill.trait$genus)){
     # g<-"POA"
-    # first summarize mean trait value for each species, then...
+    # first summarize mean trait value for each species, then summarize mean trait value for each genus 
     original.traits<-all.traits2%>%filter(TraitName1==tr & genus==g)%>%group_by(TraitName1, genus, standard_taxon)%>%
       dplyr::summarise(trait_value_avg_species=mean(StdValue))%>%
       group_by(genus)%>%dplyr::summarise(trait_value_avg=mean(trait_value_avg_species))
@@ -481,8 +422,7 @@ sp.and.genus.level.traits.nutnet<-sp.level.trait.nutnet%>%mutate(data.type="trai
 table(sp.and.genus.level.traits.nutnet$TraitName1)
 
 # save the data 
-# save(sp.and.genus.level.traits.nutnet, file="species and genus level traits from TRY, Aus, and BIEN for NutNet species.rdata")
-# fwrite(sp.and.genus.level.traits.nutnet, file="species and genus level traits from TRY, Aus, and BIEN for NutNet species.csv")
+# fwrite(sp.and.genus.level.traits.nutnet, file="species and genus level traits from TRY, Aus, BIEN, and NutNet for NutNet species.csv")
 
 ###################################################################################################
 ######################check trait values, distributions, and correlations##########################
@@ -503,17 +443,17 @@ sp.nutnet_12$extreme.values<-factor(sp.nutnet_12$extreme.values, levels = c("Yes
 (pp.distribution<-ggplot(sp.nutnet_12)+theme_bw(base_size = 20)+
     geom_density(aes(x=avg.value, color=extreme.values, fill=extreme.values))+
     facet_wrap(~TraitName1, scales="free"))
-# ggsave(pp.distribution, width=13.3,height=6.64, file="distribution of trait values including and excluding extreme values.png")
+ ggsave(pp.distribution, width=13.3,height=6.64, file="distribution of trait values including and excluding extreme values.png")
 
 
-# change data to wide version, focus on traits with high coverage for species 
+# change data to wide version, focus on traits with high coverage for species (leaf K was deleted)
 sp.nutnet.w<-sp.nutnet2%>%mutate(data.type=NULL, extreme.values=NULL)%>%filter(TraitName1 %in%c("Height", "Seed dry mass", "SLA", "LA", "Leaf N", "Leaf C", "Leaf P"))%>%
   pivot_wider(names_from = TraitName1, values_from = avg.value)
 
-colnames(sp.nutnet.w)<-c("standard_taxon", "Leaf N", "Height",  "LA",  "Leaf P", "SLA", "Seed mass",  "Leaf C")
+colnames(sp.nutnet.w)<-c("standard_taxon", "Leaf N", "Height",  "LA",  "Leaf P", "Seed mass",  "SLA",  "Leaf C")
 (pp1<-ggpairs(sp.nutnet.w[,c("Height", "Seed mass", "LA", "Leaf C", "Leaf N", "Leaf P",  "SLA")])+theme_bw(base_size = 20)+
     labs(title=paste0("pairwise correlation based on ", nrow(sp.nutnet.w), " NutNet species (in total ", length(all.spp), " species occurred)")))
-# ggsave(pp1, width=13.3,height=6.64, file="correlation among traits.png")
+ ggsave(pp1, width=13.3,height=6.64, file="correlation among traits.png")
 
 ## check PCA plot using 6 traits with all traits 
 sp.nutnet.w.na.omit<-sp.nutnet.w %>% filter_at(vars("Height", "Seed mass", "LA", "Leaf C", "Leaf N", "Leaf P", "SLA"),all_vars(!is.na(.)))
@@ -523,7 +463,7 @@ summary(pca.r)##
 # visualize 
 fviz_pca_var(pca.r, label = "var", alpha.ind =0.5, pointsize=2, title = NULL)+theme_bw(base_size=18)
 (pp2<-fviz_pca_biplot(pca.r, label = "var", alpha.ind =0.5, pointsize=2, title = paste0("PCA based on ", nrow(sp.nutnet.w.na.omit), " species having all 7 traits (in total ", length(all.spp), " species occurred)"))+theme_bw(base_size=18))
-# ggsave(pp2, width=13.3,height=6.64, file="pca among traits.png")
+ ggsave(pp2, width=13.3,height=6.64, file="pca among traits.png")
 
 # there are two sets of traits, related to size and leaf traits 
 # including all these traits reduces number of species strongly. 
@@ -533,59 +473,20 @@ fviz_pca_var(pca.r, label = "var", alpha.ind =0.5, pointsize=2, title = NULL)+th
 trait.inf1<-sp.nutnet2%>%group_by(TraitName1)%>%summarise(percent.species.with.traits=length(avg.value)/length(all.spp))
 trait.inf2<-sp.nutnet2%>%group_by(TraitName1, data.type)%>%summarise(percent.species.with.traits=length(avg.value)/length(all.spp))%>%
   pivot_wider(names_from = data.type, values_from = percent.species.with.traits)
-colnames(trait.inf2)<-c("TraitName1", "percent.species.with.traits.at.genue.level", "percent.species.with.traits.at.species.level")
 trait.inf3<-all.traits1%>%filter(standard_taxon%in%d7$standard_taxon)%>%select(database, standard_taxon, TraitName1, StdValue)%>%distinct()%>%
   group_by(database, standard_taxon, TraitName1)%>%summarise(avg.value=mean(StdValue))%>%
   group_by(TraitName1, database)%>%summarise(percent.species.with.traits=length(avg.value)/length(all.spp))%>%
   pivot_wider(names_from = database, values_from = percent.species.with.traits)
-colnames(trait.inf3)<-c("TraitName1", "percent.species.with.traits.at.species.level.from.TRY", "percent.species.with.traits.at.species.level.from.AusTrait", "percent.species.with.traits.at.species.level.from.BEIN.data",  "percent.species.with.traits.at.species.level.from.NutNet")
 trait.inf4<-trait.inf1%>%merge(y=trait.inf2, by=c("TraitName1"))%>%merge(y=trait.inf3, by=c("TraitName1"))%>%
   arrange(-percent.species.with.traits)
 trait.inf4[,2:8]<-lapply(trait.inf4[,2:8], function(x){x*100} )
 trait.inf4[,2:8]<-round(trait.inf4[,2:8], 0)
+colnames(trait.inf4)<-c("TraitName", "percent species with traits",                                
+                       "percent species with traits at genue level",                  "percent species with traits at species level",               
+                       "percent species with traits at species level from TRY",       "percent species with traits at species level from AusTrait" ,
+                       "percent species with traits at species level from BEIN data",  "percent species with traits at species level from NutNet" )  
+                    
+ 
 # fwrite(trait.inf4, file="summary of trait information.csv")
  
-###################################################################################################
-#############################cwm and fd  at the plot level over time###############################
-###################################################################################################
-# add species with traits to the cover data 
-# focus on cover data from year_trt 1, 
-# delete plots with total cover >200, does not seem realisitc 
-colnames(d7);colnames(sp.nutnet2)
-d8.l<-d7%>%filter(max_cover>0)%>%select(site_code, trt, Taxon, block, plot, subplot, trt, year_trt , year,  max_cover)%>%unique()%>%
-  group_by(site_code, block,plot, subplot,  trt, year_trt)%>%mutate(total_cover=sum(max_cover))%>%
-   filter(year_trt>0 & trt %in% c("Control", "NPK"))%>%
-  mutate(plot.id=paste0(site_code, block, plot, subplot, trt, year_trt), standard_taxon=Taxon)%>%
-  merge(sp.nutnet2, by=c("standard_taxon"))%>%filter(TraitName1%in% c("Height", "Seed dry mass", "SLA", "LA", "Leaf N", "Leaf C", "Leaf P"))
-hist(d8.l$total_cover)
-check.na.t<-d8.l[is.na(d8.l$avg.value),]
-check.na.c<-d8.l[is.na(d8.l$max_cover),]
-
-
-fun<-c()
-for(tr in unique(d8.l$TraitName1)){
- for (pl in unique(d8.l$plot.id)){
-  # pl<-"azi.cn11CControl1"; tr<-"Leaf N"
-  temp.data<-d8.l%>%filter(plot.id==pl & TraitName1==tr)
-  sp.tr<-temp.data$avg.value
-  names(sp.tr)<-temp.data$standard_taxon
-  abund1<-temp.data$max_cover
-  names(abund1)<-temp.data$standard_taxon
-  total_cover_traits<-sum(abund1) # total cover with traits
-  # 1 species is not possible to calculate fd, but possible to calculate cwm, two species with same trait value is not possible either
-  if(length(unique(abund1))<2 | length(unique(sp.tr))<2) 
-    next
-  (fd.temp <- dbFD(sp.tr, abund1, calc.FRic = F, calc.FDiv = F, w.abun=T))##  
-  temp.traits<-data.frame(plot.id=pl, f.dispersion=fd.temp$FDis, cwm1=fd.temp$CWM[,1],
-                         f.richness=fd.temp$sing.sp, total_cover_traits=total_cover_traits, trait.name=tr)
-  fun<-rbind(fun, temp.traits)
- }
-}
-
-# check the traits used 
-colnames(fun)
-fun1<-d8.l%>%select(site_code, block, plot, subplot, trt, year_trt, plot.id, total_cover)%>%distinct()%>%
-  merge(fun, by=c("plot.id"))
-table(fun1$trait.name)
-
-# fwrite(fun1, file="functional dispersion and community weighted mean at plot level.csv")
+# the end 
